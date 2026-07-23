039b708894e86a80b128cde84ba426945e1708d7
rockbenben <qingwhat@gmail.com>
2026-07-24 05:54:38 +0800

    feat: add error boundary and chunk-load auto-reload; update i18n strings and screenshot assets

--- src/app/[locale]/MDTranslator.tsx
+++ src/app/[locale]/MDTranslator.tsx
@@ -427,7 +427,7 @@ const MDTranslator = () => {
   };
 
   return (
-    <Spin spinning={isFileProcessing} description="Please wait..." size="large">
+    <Spin spinning={isFileProcessing} description={t("pleaseWait")} size="large">
       <Row gutter={[24, 24]}>
         {/* Left Column: Upload and Main Actions */}
         <Col xs={24} lg={14} xl={15}>
